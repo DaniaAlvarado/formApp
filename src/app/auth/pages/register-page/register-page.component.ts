7e93498bd1a38af7f38a20005290e5ae3f72b059
DaniaAlvarado <dania26alvarado@gmail.com>
2024-12-04 19:35:03 -0500

    fin sección 18

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+//import * as customValidators from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/service/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,4 +10,34 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  private fb: FormBuilder = new FormBuilder();
+
+  constructor(
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidatorService
+   ){}
+
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern ) ]],
+    //email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern )], [ new EmailValidatorService()]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern )], [ this.emailValidator]],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6) ]],
+    password2: ['', [Validators.required]],
+  }, {
+    validators: [
+      this.validatorsService.isFieldOneEqualFieldTwo('password', 'password2')
+    ]
+  });
+
+
+
+  isValidField(field: string){
+    return this.validatorsService.isValidfield(this.myForm, field);
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
 }
